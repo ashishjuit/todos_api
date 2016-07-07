1ddeb9192c32405c8046bfa3dc845bb16b8a3bfd
ASHISH AGGARWAL <ashishjyc@gmail.com>
2016-07-06 20:42:13 -0400

    refactored to use external model

--- models/todo.js
+++ models/todo.js
@@ -1,16 +1,20 @@
 var uuid = require('uuid');
 //Constructor
-//How do I bild this object?
+//How do I build this object?
 function Todo(description){
   this.id=uuid.v4();
   this.description = description;
   this.isComplete = false;
 }
 
-//sandbox
-// var todo1 = new Todo('read my book');
-// var todo2 = new Todo('practice programming');
-// var todo2 = new Todo('go running....');
-// console.log(todo1);
+Todo.prototype.updateComplete = function(value){
+  this.isComplete = value;
+};
 
-module.exports=Todo;
+//sandbox
+var todo1 = new Todo('read my book');
+var todo2 = new Todo('practice programming');
+var todo3 = new Todo('go running....');
+console.log(todo1);
+
+module.exports = Todo;
